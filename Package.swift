2488642f704c02799ec53cd234c938766beeda4c
Richard Wei <rxrwei@gmail.com>
2016-10-29 20:13:16 -0500

    Revert "Add library targets"
    This reverts commit f6f52b67e0e5d1f20b018786a90d39ef53fc4b9d.

--- Package.swift
+++ Package.swift
@@ -3,16 +3,3 @@ import PackageDescription
 let package = Package(
     name: "CCUDA"
 )
-
-let dylib = Product(
-    name: "CUDA",
-    type: .Library(.Dynamic),
-    modules: [ "CCUDA", "CCUDARuntime", "CNVRTC", "CCuBLAS", "CNVBLAS", "CCuDNN" ]
-)
-products.append(dylib)
-let staticLib = Product(
-    name: "CUDA",
-    type: .Library(.Static),
-    modules: [ "CCUDA", "CCUDARuntime", "CNVRTC", "CCuBLAS", "CNVBLAS", "CCuDNN" ]
-)
-products.append(staticLib)
